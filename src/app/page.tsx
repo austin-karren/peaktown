5d5774865d91cec693e916a7396a34a59c495de2
Austin Karren <austin.t.karren@gmail.com>
2024-02-16 20:09:51 -0700

    custom button with catalyst styles and types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 
 import { CreatePost } from "@peaktown/app/_components/create-post";
 import { api } from "@peaktown/trpc/server";
+import { Button } from "@peaktown/components/button";
 
 export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <Button>new button</Button>
+      <Button href="/link">Link button</Button>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
